3c0e24e523cf6a23ad0bd85b3c528386a67279c0
Kim Mun Su <doldoly0723@naver.com>
2023-11-16 03:11:57 +0900

    listen,bind,accept

---
+++ Server/Server.cpp
@@ -0,0 +1,84 @@
+#include "Common.h"
+
+
+#define SERVERPORT 9000
+
+// 클라이언트를 처리하는 쓰레드
+DWORD WINAPI ProcessClient1(LPVOID arg) 
+{
+
+}
+
+DWORD WINAPI ProcessClient2(LPVOID arg)
+{
+
+}
+
+DWORD WINAPI ProcessRoom(LPVOID arg)
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+	int retval;
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	// 소켓 생성
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET) err_quit("socket()");
+
+	// bind()
+	struct sockaddr_in serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = bind(listen_sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) err_quit("bind()");
+
+	// listen()
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR) err_quit("listen()");
+
+	SOCKET client_sock;
+	struct sockaddr_in clientaddr;
+	int addrlen;
+	HANDLE hThread;
+
+	while (1) {
+		// accept()
+		addrlen = sizeof(clientaddr);
+		client_sock = accept(listen_sock, (struct sockaddr*)&clientaddr, &addrlen);
+		if (client_sock == INVALID_SOCKET) {
+			err_display("accept()");
+			break;
+		}
+
+		// 클라랑 잘 연결 되었는지 출력 
+		char addr[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+		printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
+			addr, ntohs(clientaddr.sin_port));
+
+		// 클라이언트 처리 담당 스레드 생성
+		hThread = CreateThread(NULL, 0, ProcessClient1, (LPVOID)client_sock, 0, NULL);
+		if (hThread == NULL) { 
+			closesocket(client_sock); 
+		}
+		else { 
+			CloseHandle(hThread); 
+		}
+
+		// 대기중인 인원이 2명인가?
+		// no->반복, yes -> 방 처리 스레드 생성
+	}
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
